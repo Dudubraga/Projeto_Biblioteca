30931efc144fb69eb7bc6dff3a6b768542857c73
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-10-17 08:58:01 -0300

    botao clicavel!

--- biblioteca/projeto_biblioteca/app_biblioteca/views.py
+++ biblioteca/projeto_biblioteca/app_biblioteca/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'biblioteca/home.html')
+
+def cadastro(request):
+    return render(request,'biblioteca/cadastro.html')
--- biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
+++ biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app_biblioteca import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='aba_cadastro')
 ]
