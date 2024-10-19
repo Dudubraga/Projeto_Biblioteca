c3ea9778216f166fbfc091deb7d118e5ebf764f2
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-10-19 20:20:50 -0300

    implementando aba_login link para aba_cadastro

--- biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
+++ biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
@@ -3,5 +3,6 @@ from app_biblioteca import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('cadastro/', views.cadastro, name='aba_cadastro')
+    path('cadastro/', views.cadastro, name='aba_cadastro'),
+    path('login/', views.login, name='aba_login')
 ]
