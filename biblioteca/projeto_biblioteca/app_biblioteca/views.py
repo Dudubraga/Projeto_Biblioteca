8ed89ace9b4560dbf47be4fc73be32f50e008487
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-10-19 21:52:22 -0300

    banco de dados de cadastro feito

--- biblioteca/projeto_biblioteca/app_biblioteca/views.py
+++ biblioteca/projeto_biblioteca/app_biblioteca/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuario
 
 def home(request):
     return render(request,'biblioteca/home.html')
@@ -8,3 +9,18 @@ def cadastro(request):
 
 def login(request):
     return render(request, 'biblioteca/login.html')
+
+def usuarios(request):
+    #salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.nascimento = request.POST.get('nascimento')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.senha = request.POST.get('senha')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'biblioteca/usuarios.html', usuarios)
--- biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
+++ biblioteca/projeto_biblioteca/projeto_biblioteca/urls.py
@@ -4,5 +4,6 @@ from app_biblioteca import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro/', views.cadastro, name='aba_cadastro'),
-    path('login/', views.login, name='aba_login')
+    path('login/', views.login, name='aba_login'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
